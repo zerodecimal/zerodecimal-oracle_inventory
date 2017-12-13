c7709dac556eb7e8b7847f9d60fee47129c53933
Max Buehler <max.buehler@nucor.com>
2017-12-13 15:09:14 -0600

    renamed client_home db_client_home

--- lib/facter/oracle_inventory.rb
+++ lib/facter/oracle_inventory.rb
@@ -10,7 +10,7 @@
     oms_home
     agent_home
    *oracle_emagent (associative array of agent home parameter/value pairs)
-    client_home
+    db_client_home
     ebs_env_home
     wls_home
     cluster_nodes (comma-delimited list of RAC cluster nodes)
@@ -136,8 +136,8 @@ begin
                   o_inventory['ebs_env_home'] = home_dir.sub(/\/fs.*/, '')
                 when /^oracle.*(wls|coherence)/            ## WLS_HOME
                   o_inventory['wls_home'] = home_dir
-                when /Oracle Client/i                      ## CLIENT_HOME
-                  o_inventory['client_home'] = home_dir
+                when /Oracle Client/i                      ## DB_CLIENT_HOME
+                  o_inventory['db_client_home'] = home_dir
                 end
               end
               h_inventory.clear
