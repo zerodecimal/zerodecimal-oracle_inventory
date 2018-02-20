b5348bd42cc2ed25a3f3be1bfcc95cb398cb531b
Eric Miller <emiller1020@gmail.com>
2018-02-20 10:18:49 -0600

    Rewrite of custom fact script to standardize fact names

--- lib/facter/oracle_inventory.rb
+++ lib/facter/oracle_inventory.rb
@@ -1,46 +1,43 @@
 # oracle_inventory.rb
 
 =begin
+
   This script will inspect the central Oracle Inventory
   and return the following entries, if they exist:
-    oracle_home (semicolon-delimited list of Oracle database homes)
-   *oracle_db (associative array of db home parameter/value pairs)
-    grid_home
-   *oracle_crs (associative array of grid home parameter/value pairs)
-    oms_home
-    agent_home
-   *oracle_emagent (associative array of agent home parameter/value pairs)
-    db_client_home
-    ebs_env_home
-    wls_home
-    cluster_nodes (comma-delimited list of RAC cluster nodes)
-
-    [* = These facts return non-string objects, which was not possible before 2015.3]
+    oracle_inventory (string): Central inventory file location
+    oracle_crs_home (hash): CRS home information
+    oracle_rac_nodes (array): List of RAC cluster nodes
+    oracle_db_home (hash): Database home information
+    oracle_oms_home (hash): OMS (Enterprise Manager) home information
+    oracle_em_agent_home (hash): OEM Agent home information
+    oracle_ebs_home (hash): EBS application home information
+    oracle_wls_home (hash): WebLogic home information
+    oracle_client_home (hash): Database Client information
 
   The Inventory location is defined here:
     UNIX: /var/opt/oracle/oraInst.loc
     LINUX: /etc/oraInst.loc
   On Windows the inventory is always C:/Program Files/Oracle/Inventory/ContentsXML/inventory.xml
+
 =end
 
 begin
+
   ## Required GEMs
   require 'facter'
   require 'xmlsimple'
+  require 'time'
+
   ## Global variables
   oratab      = {}
   ora_inv_loc = Facter.value(:kernel) =~ /linux/i ? '/etc/oraInst.loc'
-              ## There are currently no plans to manage any Unix servers
-              #: Facter.value(:kernel) =~ /unix/i  ? '/var/opt/oracle/oraInst.loc'
+              : Facter.value(:kernel) =~ /unix/i  ? '/var/opt/oracle/oraInst.loc'
               :                                     nil
   ora_inv     = nil
-  db_homes    = []
-  o_inventory = {
-    'oracle_db'      => {},
-    'oracle_emagent' => {},
-  }
+  o_inventory = {}
+
   ## Find the Central Inventory location if we are not on a Windows platform
-  if (ora_inv_loc and File.readable?(ora_inv_loc))
+  if (!ora_inv_loc.nil? and File.readable?(ora_inv_loc))
     IO.foreach(ora_inv_loc) do |line|
       line[/^inventory_loc=(.+)$/] && ora_inv = $1 + '/ContentsXML/inventory.xml'
     end
@@ -48,18 +45,56 @@ begin
   elsif Facter.value(:osfamily) =~ /windows/i
     ora_inv = 'C:/Program Files/Oracle/Inventory/ContentsXML/inventory.xml'
   end
-  ## Cache the DB home entries from /etc/oratab (ignore ASM)
+
+  ## Cache the DB home and SID information from /etc/oratab (ignore ASM)
   if File.readable?('/etc/oratab')
     File.open('/etc/oratab','r') do |otab_file|
       otab_file.each_line do |line|
         next unless line[/^[a-z]/i]
         entry = line.split(':')
-        oratab[entry[1]] = entry[0]
+        oratab.has_key?(entry[1]) || oratab[entry[1]] = []
+        oratab[entry[1]] << entry[0] && oratab[entry[1]].sort!
       end
     end
   end
+
+  ## Parse a block of XML and return PSU version/install time data
+  ## Parameters:
+  ##   oneoff_list (array): the XML array
+  def get_oneoff_info (oneoff_list)
+    times = []
+    patches = {}
+    data = {
+      'ver'       => '',
+      'inst_time' => '',
+    }
+    oneoff = oneoff_list[0]['ONEOFF'] || []
+    oneoff.each do |patch|
+      if patch['DESC'][0][/^(?:OCW|Database) Patch Set Update : (\S+)/]
+        patches[$1] = patch['INSTALL_TIME']
+        times << patch['INSTALL_TIME']
+      end
+    end
+    if patches.size > 0
+      ## Here we have to sort the patches by install time to get the newest one
+      if patches.size > 1
+        ## This sorts the times array by date/time
+        sorted = times.sort_by {|t| Time.parse(t)}
+        ## This reduces the patches hash to the newest one
+        patches.select! {|k,v| v == sorted[-1]}
+        data['ver'] = patches.keys[0]
+        data['inst_time'] = patches.values[0]
+      else
+        data['ver'] = patches.keys[0]
+        data['inst_time'] = patches.values[0]
+      end
+    end
+  return data
+  end
+
   ## Parse the Central Inventory and begin setting the Fact variables
   if (ora_inv and File.readable?(ora_inv))
+    o_inventory['oracle_inventory'] = ora_inv
     c_inventory = XmlSimple.xml_in(ora_inv)
     if c_inventory['HOME_LIST'][0]['HOME']
       c_inventory['HOME_LIST'].each do |list|
@@ -68,83 +103,124 @@ begin
             home_dir       = home['LOC']
             home_inv_props = home_dir + '/inventory/ContentsXML/oraclehomeproperties.xml'
             home_inv_comps = home_dir + '/inventory/ContentsXML/comps.xml'
-            if home['CRS']
-              ## GRID_HOME *note* This can also be found in /etc/oracle/olr.loc
-              o_inventory['grid_home'] = home_dir
-              if File.readable?(home_inv_comps)
-                h_inventory = XmlSimple.xml_in(home_inv_comps)
-                if h_inventory['TL_LIST'][0]['COMP']
-                  o_inventory['oracle_crs'] = {
-                    home_dir => {
-                      'ver'       => h_inventory['TL_LIST'][0]['COMP'][0]['VER'],
-                      'inst_time' => h_inventory['TL_LIST'][0]['COMP'][0]['INSTALL_TIME'],
-                    }
-                  }
-                end
-                h_inventory.clear
-              end
-              ## This is a RAC node, go to the GRID_HOME Inventory and get the list of cluster nodes
-              if File.readable?(home_inv_props)
-                g_inventory = XmlSimple.xml_in(home_inv_props)
-                if g_inventory['CLUSTER_INFO']
-                  node_list = []
-                  g_inventory['CLUSTER_INFO'].each do |cluster|
-                    cluster['NODE_LIST'].each do |list|
-                      list['NODE'].each do |node|
-                        node_list.push(node['NAME'])
-                      end
-                    end
-                  end
-                  if node_list.length
-                    o_inventory['cluster_nodes'] = node_list.sort.join(',')
-                  end
-                end
-                g_inventory.clear
-              end
-            elsif File.readable?(home_inv_comps)
+            if File.readable?(home_inv_comps)
               h_inventory = XmlSimple.xml_in(home_inv_comps)
-              if h_inventory['TL_LIST'][0]['COMP']
-                case h_inventory['TL_LIST'][0]['COMP'][0]['EXT_NAME'][0]
-                when /^Oracle Database\s+\d/i              ## DB_HOME
-                  db_homes.push(home_dir)
-                  psu_ver = ''
-                  psu_inst_time = ''
-                  if h_inventory['ONEOFF_LIST'][0]['ONEOFF']
-                    h_inventory['ONEOFF_LIST'][0]['ONEOFF'].each do |patch|
-                      if patch['DESC'][0] =~ /^Database Patch Set Update : ([\d\.]+) /
-                        psu_ver = $1
-                        psu_inst_time = patch['INSTALL_TIME']
+              all_comps = h_inventory['TL_LIST'][0]['COMP'] || []
+              if all_comps.length > 0
+                all_comps.each_index do |comp_i|
+                  case all_comps[comp_i]['NAME']
+                  ## CRS Home *note* This can also be found in /etc/oracle/olr.loc
+                  when 'oracle.crs'
+                    ## Get the PSU information
+                    psu_ver = ''
+                    psu_inst_time = ''
+                    oneoff_list = h_inventory['ONEOFF_LIST'] || []
+                    if oneoff_list.length > 0
+                      psu_data = get_oneoff_info(oneoff_list)
+                      if psu_data.size > 0
+                        psu_ver = psu_data['ver']
+                        psu_inst_time = psu_data['inst_time']
                       end
                     end
+                    ## There can be only one
+                    o_inventory['oracle_crs_home'] = {
+                      home_dir => {
+                        'ver'           => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                        'inst_time'     => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                        'psu_ver'       => psu_ver,
+                        'psu_inst_time' => psu_inst_time,
+                      }
+                    }
+                    ## Get the list of cluster nodes
+                    if File.readable?(home_inv_props)
+                      g_inventory = XmlSimple.xml_in(home_inv_props)
+                      cluster_info = g_inventory['CLUSTER_INFO'] || []
+                      if cluster_info.length > 0
+                        all_nodes = []
+                        node_list = cluster_info[0]['NODE_LIST'] || []
+                        if node_list.length > 0
+                          node_list[0]['NODE'].each do |node|
+                            all_nodes << (node['NAME'])
+                          end
+                        end
+                        if all_nodes.length > 0
+                          o_inventory['oracle_rac_nodes'] = all_nodes.sort
+                        end
+                      end
+                      g_inventory.clear
+                    end
+                    break
+                  ## DB Home
+                  when 'oracle.server'
+                    ## Get the PSU information
+                    psu_ver = ''
+                    psu_inst_time = ''
+                    oneoff_list = h_inventory['ONEOFF_LIST'] || []
+                    if oneoff_list.length > 0
+                      psu_data = get_oneoff_info(oneoff_list)
+                      if psu_data.size > 0
+                        psu_ver = psu_data['ver']
+                        psu_inst_time = psu_data['inst_time']
+                      end
+                    end
+                    o_inventory.has_key?('oracle_db_home') || o_inventory['oracle_db_home'] = {}
+                    o_inventory['oracle_db_home'][home_dir] = {
+                      'ver'           => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                      'inst_time'     => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                      'psu_ver'       => psu_ver,
+                      'psu_inst_time' => psu_inst_time,
+                      'sid'           => oratab[home_dir] || [],
+                    }
+                    break
+                  ## OMS Home
+                  when 'oracle.sysman.top.oms'
+                    ## There can be only one
+                    o_inventory['oracle_oms_home'] = {
+                      home_dir => {
+                        'ver'       => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                        'inst_time' => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                      }
+                    }
+                    break
+                  ## EM Agent Home
+                  when 'oracle.sysman.top.agent'
+                    ## There can be only one
+                    o_inventory['oracle_em_agent_home'] = {
+                      home_dir => {
+                        'ver'       => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                        'inst_time' => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                      }
+                    }
+                    break
+                  ## EBS Home
+                  when 'oracle.apps.ebs'
+                    ## There can be only one
+                    o_inventory['oracle_ebs_home'] = {
+                      home_dir.sub(/\/fs.*/, '') => {
+                        'ver'       => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                        'inst_time' => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                      }
+                    }
+                    break
+                  ## WebLogic Home
+                  when /^oracle\.(wls\.clients|coherence)$/
+                    o_inventory.has_key?('oracle_wls_home') || o_inventory['oracle_wls_home'] = {}
+                    o_inventory['oracle_wls_home'][home_dir] = {
+                      'ver'       => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                      'inst_time' => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                    }
+                    break
+                  ## Client Home
+                  when 'oracle.client'
+                    o_inventory.has_key?('oracle_client_home') || o_inventory['oracle_client_home'] = {}
+                    o_inventory['oracle_client_home'][home_dir] = {
+                      'ver'       => h_inventory['TL_LIST'][0]['COMP'][comp_i]['VER'],
+                      'inst_time' => h_inventory['TL_LIST'][0]['COMP'][comp_i]['INSTALL_TIME'],
+                    }
+                    break
+                  else
+                    next
                   end
-                  o_inventory['oracle_db'][home_dir] = {
-                    'ver'           => h_inventory['TL_LIST'][0]['COMP'][0]['VER'],
-                    'inst_time'     => h_inventory['TL_LIST'][0]['COMP'][0]['INSTALL_TIME'],
-                    'psu_ver'       => psu_ver,
-                    'psu_inst_time' => psu_inst_time,
-                    'sid'           => oratab[home_dir] || '',
-                  }
-                when /^(EM Platform \(OMS\)|oracle.sysman.top.oms)/i ## OMS_HOME
-                  o_inventory['oms_home'] = home_dir
-                ## This garbage is in here until we figure out a better way to
-                ## identify the software in an Oracle Home. It's a result of the
-                ## weird OMS inventory on rzslca940.
-                when /^oracle\.classloader/i
-                  if h_inventory['TL_LIST'][0]['COMP'][0]['INV_LOC'][/oem/i]
-                    o_inventory['oms_home'] = home_dir
-                  end
-                when /^EM Platform \(Agent\)/i             ## AGENT_HOME
-                  o_inventory['agent_home'] = home_dir
-                  o_inventory['oracle_emagent'][home_dir] = {
-                    'ver'       => h_inventory['TL_LIST'][0]['COMP'][0]['VER'],
-                    'inst_time' => h_inventory['TL_LIST'][0]['COMP'][0]['INSTALL_TIME'],
-                  }
-                when /^Oracle E-Business Suite Component/i ## EBS_ENV_HOME
-                  o_inventory['ebs_env_home'] = home_dir.sub(/\/fs.*/, '')
-                when /^oracle.*(wls|coherence)/            ## WLS_HOME
-                  o_inventory['wls_home'] = home_dir
-                when /Oracle Client/i                      ## DB_CLIENT_HOME
-                  o_inventory['db_client_home'] = home_dir
                 end
               end
               h_inventory.clear
@@ -154,19 +230,17 @@ begin
       end
     end
     c_inventory.clear
-    ## Join the db_homes into a string
-    if db_homes.length > 0
-      o_inventory['oracle_home'] = db_homes.join(';')
-    end
   end
+
   ## Add the Inventory elements to Facter
-  o_inventory.each{|name,fact|
+  o_inventory.each do |name, fact|
     Facter.add(name) do
       setcode do
         fact
       end
     end
-  }
+  end
+
 rescue Exception => e
   # The required file will not be there the first time this fact is loaded
   if e.message[/cannot load such file/i]
